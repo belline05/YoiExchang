b11d05bcd00e01adc16a4123256b4a3daa30ab82
bb <bellep38@hotmail.com>
2017-12-03 10:33:40 +0700

    CMS

--- app/src/main/java/com/example/administrator/yoiexchang/MainActivity.java
+++ app/src/main/java/com/example/administrator/yoiexchang/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.administrator.yoiexchang;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.administrator.yoiexchang.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+
+//        add fragment to activity
+        if (savedInstanceState == null){
+//            Condition is True
+         getSupportFragmentManager()
+                 .beginTransaction()
+                 .add(R.id.contentMainFragment, new MainFragment())
+                 .commit();
+
+
+        } //if
+    }   //main method
+}   //main class
